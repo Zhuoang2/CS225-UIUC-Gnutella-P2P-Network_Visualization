7b09995fbb1188d93a9aeaa31110c8378a186e69
Zhuoang2 <90300558+Zhuoang2@users.noreply.github.com>
2022-11-27 20:22:51 -0600

    Delete Final_Project directory

--- Final_Project/build/src/CMakeFiles/src.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsrc.a"
-  "libsrc.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
